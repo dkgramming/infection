de2d4325a8042c016afeee920cf2ed0df3007374
David Kisluk <david.kisluk@gmail.com>
2015-02-14 00:08:11 -0500

    Add test stubs for adding students to a coach

--- user_test.rb
+++ user_test.rb
@@ -7,4 +7,16 @@ class UserTest < Test::Unit::TestCase
     new_user = User.new(0)
     assert_not_nil new_user.site_version, "This user has no site version number."
   end
+
+  def test_add_one_student
+    assert false, "Test not implemented"
+  end
+
+  def test_add_many_students
+    assert false, "Test not implemented"
+  end
+
+  def test_infect_one_student
+    assert false, "Test not implemented"
+  end
 end
