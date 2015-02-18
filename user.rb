61fe59f7d141b1e09ecbbd689c1adf130f9ea428
David Kisluk <david.kisluk@gmail.com>
2015-02-18 03:23:34 -0500

    Modify total_infection to work for both coaches and is_coached_by relations

--- user.rb
+++ user.rb
@@ -2,14 +2,17 @@ class User
   @site_version
   @students
   @coach
+  @infected
   attr_accessor :site_version
   attr_accessor :students
   attr_accessor :coach
+  attr_accessor :infected
 
   def initialize (version)
     @site_version = version
     @students = []
     @coach = nil
+    @infected = false
   end
 
   def add_student (student)
@@ -18,11 +21,19 @@ class User
   end
 
   def total_infection
-    @students.each do |student| 
-      student.site_version = self.site_version
+    if not @infected then
+      @infected = true
 
-      # Recursively infected students 
-      student.total_infection()
+      @students.each do |student| 
+        student.site_version = self.site_version
+
+        # Recursively infected students 
+        student.total_infection()
+      end
+
+      @coach.total_infection() unless @coach.nil?
+
+      @infected = false
     end
   end
 end
